cd3d0c2c761971d1c2597427917e6fb8ab4e85cd
Chandru Rajendiran <Chandru.Rajendiran@gisbizdev.gisbiz.chennai.com>
2023-11-07 16:40:35 +0530

    push

---
+++ CoreMVCClient/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using CoreMVCClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace CoreMVCClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //Iconfiguration Responsible to get appsetting.json
+        private readonly IConfiguration _config;
+        private readonly string apiBaseUrl;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+            apiBaseUrl = _config.GetValue<string>("WebAPIBaseUrl");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Account> accounts = new List<Account>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiBaseUrl + "/Accounts"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    accounts = JsonConvert.DeserializeObject<List<Account>>(apiResponse);
+                }
+            }
+            return View(accounts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
